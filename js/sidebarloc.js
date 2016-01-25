554ffbde9073570d706c4263d271eceda988056d
Damon Maldonado <damon@mpwmarketing.com>
2016-01-25 14:13:03 -0500

    clean up

--- js/sidebarloc.js
+++ js/sidebarloc.js
@@ -3,7 +3,6 @@ function jqMoveUpdateSize(){
     // Get the dimensions of the viewport
     var width = jQuery(window).width();
     var height = jQuery(window).height();
-    console.log(width);
     return width;
 };
 var is_moved = false;
@@ -16,11 +15,9 @@ jQuery(document).ready(function(){
     }
     jQuery(window).resize(function() {
         if (jqMoveUpdateSize() < 761 && !is_moved){
-                console.log('resize small not moved');
         		jQuery('.page-content').prepend(jQuery('#conv-buttons'));
     			is_moved = true;
         } else if (jqMoveUpdateSize() > 760 && is_moved){
-                console.log('resize large accordion');
         		jQuery('.widget-1').prepend(jQuery('#conv-buttons'));
     			is_moved = false;
         }
