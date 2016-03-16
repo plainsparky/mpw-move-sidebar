79dbc590cd8c39cdc980feb630bb438f995ef49d
damonmaldonado <damon@mpwmarketing.com>
2016-03-16 09:51:26 -0400

    changed to use class

--- js/sidebarloc.js
+++ js/sidebarloc.js
@@ -11,18 +11,18 @@ jQuery(document).ready(function(){
     if (width < 761){
         console.log('mobile');
         if (jQuery('article.accordion').length) {
-            jQuery('article.accordion').prepend(jQuery('#text-41'));
+            jQuery('article.accordion').prepend(jQuery('.contact-widget'));
     	    is_moved = true;
         }
     }
     jQuery(window).resize(function() {
         if (jqMoveUpdateSize() < 761 && !is_moved){
             if (jQuery('article.accordion').length) {
-        		jQuery('article.accordion').prepend(jQuery('#text-41'));
+        		jQuery('article.accordion').prepend(jQuery('.contact-widget'));
     			is_moved = true;
             }
         } else if (jqMoveUpdateSize() > 760 && is_moved){
-        		jQuery('.sidebar').append(jQuery('#text-41'));
+        		jQuery('.sidebar').append(jQuery('.contact-widget'));
     			is_moved = false;
         }
     });
